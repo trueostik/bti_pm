0c68f711cf4d42da364155c36fe1f1392c54e18e
true_ostik <os.kotovskyi@gmail.com>
2024-03-10 19:03:53 +0200

    default priority changed

--- bti_pm/settings.py
+++ bti_pm/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'bti_pm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if ENVIRONMENT == 'testserv1':
+"""if ENVIRONMENT == 'testserv':
   # Налаштування для PostgreSQL
     DATABASES = {
         'default': {
@@ -91,13 +91,13 @@ if ENVIRONMENT == 'testserv1':
         }
     }
 else:
-    # Налаштування для SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+  """  # Налаштування для SQLite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
